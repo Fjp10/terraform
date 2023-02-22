c9a5d455a1c3b43372d5817f47973ba8e17646ad
Fjp10 <68808298+Fjp10@users.noreply.github.com>
2023-02-22 17:26:55 +0530

    Delete vars.tf

--- vars.tf
+++
@@ -1,9 +0,0 @@
-variable "engine" {}
-variable "identifier" {}
-variable "engine_version" {}     
-variable "instance_class" {}
-variable "db_name"  {}       
-variable "username" {}  
-variable "password" {} 
-variable "parameter_group_name" {}
-variable "skip_final_snapshot" {}
