9b6b693701f75b7e6823f6b7eaed5a7ff3fbd6d6
Fjp10 <68808298+Fjp10@users.noreply.github.com>
2023-02-22 16:14:43 +0530

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "engine" {}
+variable "identifier" {}
+variable "engine_version" {}     
+variable "instance_class" {}
+variable "db_name"  {}       
+variable "username" {}  
+variable "password" {} 
+variable "parameter_group_name" {}
+variable "skip_final_snapshot" {}
