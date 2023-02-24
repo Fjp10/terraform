335fa24eef8be2c040bfb23e56f8938537ed5716
Fjp10 <68808298+Fjp10@users.noreply.github.com>
2023-02-24 15:51:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "ubuntu" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = var.instance_type
   tags = {
