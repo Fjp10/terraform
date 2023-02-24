3c30db6c9a1d9fe3488dbcd1518aba9dcdec2f1f
Fjp10 <68808298+Fjp10@users.noreply.github.com>
2023-02-24 15:46:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,37 +2,10 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = var.instance_type
-
   tags = {
     Name = var.instance_name
-  }
-}
-
-resource "aws_s3_bucket" "piyush_rds_snapshots" {
-  bucket = "piyush-rds-snap"
-  acl    = "private"
-
-  tags = {
-    Name        = "RDS snapshots"
-    Environment = "dev"
   }
 }
