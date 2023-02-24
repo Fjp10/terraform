50d5873acb0fd7cee4a5f2e46673a80d5f05bef4
Fjp10 <68808298+Fjp10@users.noreply.github.com>
2023-02-24 15:58:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = var.ami
   instance_type = var.instance_type
   tags = {
     Name = var.instance_name
