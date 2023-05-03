5a9280b5a5dd8811a52535727772fe58ff05e210
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-05-03 12:30:13 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,9 @@ output "instance_ami" {
 }
 
 output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+  value = aws_instance.ubuntu.id
 }
 
+output "ssh_public_key" {
+  value = tls_private_key.ssh.public_key_openssh
+}
