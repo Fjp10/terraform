47b2c596d01f788bbc2b97cfa10f10b184357b1c
Fjp10 <68808298+Fjp10@users.noreply.github.com>
2023-05-03 14:04:04 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "instance_arn" {
   value = aws_instance.ubuntu.id
 }
 
+output "public_ip" {
+  value = aws_instance.ubuntu.public_ip
+}
+
 output "ssh_public_key" {
   value = tls_private_key.ssh.public_key_openssh
 }
