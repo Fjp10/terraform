2033171a82833e5bd0a3898bdca20cfc015bb759
Fjp10 <68808298+Fjp10@users.noreply.github.com>
2023-05-25 12:33:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,21 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  tags = {
-    Name = var.instance_name
-  }
-}
-
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh-key"  # Provide a unique name for the key pair
+  public_key = tls_private_key.ssh.public_key_openssh
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.ssh_key.key_name  # Connect SSH key to the instance
+  tags = {
+    Name = var.instance_name
+  }
 }
